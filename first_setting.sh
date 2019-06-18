f81d3982cda81c388fdfce41258f93a86eede3f6
strongpapazola <48545591+strongpapazola@users.noreply.github.com>
2019-06-18 07:53:37 +0700

    Add files via upload

---
+++ first_setting.sh
@@ -0,0 +1,34 @@
+read -p "Download chrome?[y/n]" confirm
+if [ $confirm = "y" ];then
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	if [ ! "witch gdebi" ];then
+  	      echo "[*] gdebi not installed !"
+	else
+	        gdebi google-chrome-stable_current_amd64.deb
+	fi
+else
+	echo Abort.
+	exit 0
+fi
+
+sed -i 's/StartupNotify=true/StartupNotify=false/g' /usr/share/applications/org.gnome.Nautilus.desktop
+
+apt install bleachbit -y
+
+apt install ssh -y
+
+apt install proftpd -y
+
+apt install isc-dhcp-server -y
+
+apt install apache2 -y
+
+apt install php7.3 php7.3-curl php7.3-mysql -y
+
+apt install bind9 -y
+
+apt install asterisk -y
+
+apt install mysql -y
+
+apt install samba -y
